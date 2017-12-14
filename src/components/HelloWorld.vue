d1fda661209d21aff5e93384820a37c029c32d8d
yuanyuan.zhang <34528979+zhangyuan5cc@users.noreply.github.com>
2017-12-14 16:25:27 +0800

    s
    s

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+<p v-if='ok'>语文</p>
+<template v-if='ok'>
+<h1>11</h1>
+<h2>22</h2>
+<h3>33</h3>
+</template>
+
+<div v-if='Math.random()>0.5'>
+这里面是if条件满足之后执行的东西
+</div>
+<div v-else>
+</div>
+<p v-else>数学</p>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '全民直播'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
